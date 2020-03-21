f26f5a85b2487c060bf1ab0a9fde178705a8b108
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-21 22:03:56 +0530

    Update get-student-details.php

--- api/get-student-details.php
+++ api/get-student-details.php
@@ -1,7 +1,7 @@
 <?php
 // required headers
-//header("Access-Control-Allow-Origin: *");
-//header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
   
 // database connection will be here
 // include database and object files
