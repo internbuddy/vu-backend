d8cd8985df5e4b968a16c589e02517980608c44e
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-21 21:33:13 +0530

    Update get-student-details.php

--- api/get-student-details.php
+++ api/get-student-details.php
@@ -17,53 +17,37 @@ $student = new Student($db);
   
 // read products will be here
 // query products
-$stmt = $product->read();
-$num = $stmt->rowCount();
+$stmt = $student->getStudentDetails();
   
-// check if more than 0 record found
-if($num>0){
+// products array
+    $students_arr=array();
+    $students_arr["records"]=array();
   
-    // products array
-    $products_arr=array();
-    $products_arr["records"]=array();
-  
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        // extract row
-        // this will make $row['name'] to
-        // just $name only
-        extract($row);
-  
-        $product_item=array(
-            "id" => $id,
-            "name" => $name,
-            "description" => html_entity_decode($description),
-            "price" => $price,
-            "category_id" => $category_id,
-            "category_name" => $category_name
-        );
-  
-        array_push($products_arr["records"], $product_item);
-    }
-  
-    // set response code - 200 OK
-    http_response_code(200);
-  
-    // show products data in json format
-    echo json_encode($products_arr);
+// retrieve our table contents
+// fetch() is faster than fetchAll()
+// http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+// extract row
+// this will make $row['name'] to
+// just $name only
+extract($row);
+$product_item=array(
+     "id" => $id,
+     "name" => $name,
+     "email" => $email,
+     "f_m_name" => $f_m_name,
+     "dob" => $dob,
+     "mobile" => $mobile,
+      "ug_qual" => $ug_qual,
+      "cgpa" => $cgpa,
+      "reg_date_time" => $reg_date_time,
+      "status" => $status
+   );
+array_push($students_arr["records"], $product_item);
 }
-// no products found will be here
+// set response code - 200 OK
+http_response_code(200);
 
-else{
-  
-    // set response code - 404 Not found
-    http_response_code(404);
-  
-    // tell the user no products found
-    echo json_encode(
-        array("message" => "No products found.")
-    );
-}
+// show products data in json format
+echo json_encode($products_arr);
 ?>
