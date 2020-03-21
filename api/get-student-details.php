d021ea87a84713dea45c825b68fa35785879082b
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-21 16:14:51 +0530

    Create get-student-details.php

---
+++ api/get-student-details.php
@@ -0,0 +1,19 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+  
+// database connection will be here
+// include database and object files
+include_once 'config/database.php';
+include_once 'objects/student.php';
+  
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+  
+// initialize object
+$student = new Student($db);
+  
+// read products will be here
+?>
