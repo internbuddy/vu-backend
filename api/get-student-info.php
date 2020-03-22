2bb1ebe92d3c1ed071641a10af6304bcc62322af
Intern Buddy <60249122+internbuddy@users.noreply.github.com>
2020-03-22 11:18:13 +0530

    Update get-student-info.php

--- api/get-student-info.php
+++ api/get-student-info.php
@@ -1,4 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3000");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+error_reporting(0);
+
 include("config/connection.php");
 $result=mysqli_query($conn,"select * from vu_student_master where is_active='1'");
 $data = array();
