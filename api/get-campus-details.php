bfd3d6bea7db097f8e5384c61864c9574872a2b8
mishaPanjwani <62198894+mishaPanjwani@users.noreply.github.com>
2020-03-25 22:53:32 +0530

    Update get-campus-details.php

--- api/get-campus-details.php
+++ api/get-campus-details.php
@@ -1,35 +1,33 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3000");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET");
 error_reporting(0);
 
 include("config/connection.php");
 $result=mysqli_query($conn,"select * from vu_campus_master");
 $data = array();
 $count=mysqli_num_rows($result);
-if($count==0)
-{
-		$msg= "No Data Found";
-		$status=0;
-		$data="NA";
-}
-else
-{
-  while ($row = mysqli_fetch_assoc($result))
+
+if($count>0){
+	 while ($row = mysqli_fetch_assoc($result))
  	{
-			$status=1;
-			$msg="Success";
-			$data[]=$row;
+		 $data[]=$row;
 			$campus_id = $row['campus_id'];
 			$campus_name = $row['campus_name'];
 			$campus_image = $row['campus_image'];
 			$campus_status = $row['campus_status'];
 			$campus_longitude = $row['campus_longitude'];
 	  		$campus_latitude = $row['campus_latitude'];
-	}  
+		 
+	 }
+	
+	$dictionary = array("status"=>'1',"msg"=>'succeess',"data"=>$data);
+}else{
+		$status=0;
+		$msg= "No Data Found";		
+		$data="NA";
+	
+	$dictionary = array("status"=>$status,"msg"=>$msg,"data"=>$data);
 }
-$dictionary = array("status"=>$status,"msg"=>$msg,"data"=>$data);
 echo json_encode($dictionary);	  
